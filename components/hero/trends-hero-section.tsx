563926c09287ef7dd00587b68fb54bb5f6a0658d
shibu-sd <sd.shibu007@gmail.com>
2025-04-10 22:21:41 +0530

    added trends hero section

---
+++ components/hero/trends-hero-section.tsx
@@ -0,0 +1,22 @@
+import { ContainerTextFlip } from "../ui/container-text-flip";
+
+export default function HeroSection() {
+    return (
+        <section className="bg-zinc-50 py-16 md:py-32 dark:bg-transparent">
+            <div className="@container mx-auto max-w-5xl px-6">
+                <div className="text-center">
+                    <h2 className="text-balance text-4xl font-semibold lg:text-5xl flex justify-center items-center gap-3 flex-wrap">
+                        <span>Stay in sync with</span>
+                        <ContainerTextFlip
+                            words={["live stock prices", "market stories", "economic calendar", "stock heatmaps"]}
+                            textClassName="text-4xl lg:text-5xl"
+                        />
+                    </h2>
+                    <p className="mt-4">
+                        Get a real-time pulse on the market with FinAlpha's all-in-one trends dashboard.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}

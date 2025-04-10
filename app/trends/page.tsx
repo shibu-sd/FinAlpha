86df66d06a5ae1f3af1832ba9f003348b6f3134d
shibu-sd <sd.shibu007@gmail.com>
2025-04-10 22:53:58 +0530

    updated the trends page to include economic calendar widget

--- app/trends/page.tsx
+++ app/trends/page.tsx
@@ -1,6 +1,7 @@
 import FooterSection from "@/components/footer";
 import { HeroHeader } from "@/components/hero/hero-header";
 import HeroSection from "@/components/hero/trends-hero-section";
+import EconomicCalendar from "@/components/widgets/economic-calendar";
 import HeatmapWidget from "@/components/widgets/heatmap";
 import NewsWidget from "@/components/widgets/news";
 import ScreenerWidget from "@/components/widgets/screener";
@@ -26,9 +27,17 @@ export default function Home() {
 
         <hr></hr>
 
-        <div className="mt-12">
+        <div className="mt-12 mb-12">
           <ScreenerWidget />
         </div>
+
+        <hr></hr>
+
+        <div className="mt-12 mb-12">
+          <EconomicCalendar />
+        </div>
+
+        <hr></hr>
 
         <FooterSection />
       </div>

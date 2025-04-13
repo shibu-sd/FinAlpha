a3a28f2eefd99cfc5d887a5d9cf2138c7f7ea781
shibu-sd <sd.shibu007@gmail.com>
2025-04-13 12:14:38 +0530

    added quiz page

---
+++ app/quiz/page.tsx
@@ -0,0 +1,13 @@
+import Features from "@/components/features/quiz-features";
+import FooterSection from "@/components/footer";
+import { HeroHeader } from "@/components/hero/hero-header";
+
+export default function Tools() {
+    return (
+        <>
+            <HeroHeader />
+            <Features />
+            <FooterSection />
+        </>
+    );
+}

731242d8ee7ceaaff9c3e19d77feee9756c11fc0
shibu-sd <sd.shibu007@gmail.com>
2025-04-15 18:57:14 +0530

    updated blog page

---
+++ app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import Features from "@/components/features/blog-features";
+import FooterSection from "@/components/footer";
+import { HeroHeader } from "@/components/hero/hero-header";
+
+export default function Tools() {
+    return (
+        <>
+            <HeroHeader />
+            <Features />
+            <FooterSection />
+        </>
+    );
+}

9815786ecd52509e7a18d73cacad8e8e1ac0a8f5
shibu-sd <sd.shibu007@gmail.com>
2025-04-18 19:57:12 +0530

    updated trends page

--- app/trends/page.tsx
+++ app/trends/page.tsx
@@ -1,10 +1,7 @@
+import TrendsFeatures from "@/components/features/trends-features";
 import FooterSection from "@/components/footer";
 import { HeroHeader } from "@/components/hero/hero-header";
 import HeroSection from "@/components/hero/trends-hero-section";
-import EconomicCalendar from "@/components/widgets/economic-calendar";
-import HeatmapWidget from "@/components/widgets/heatmap";
-import NewsWidget from "@/components/widgets/news";
-import ScreenerWidget from "@/components/widgets/screener";
 import StripWidget from "@/components/widgets/strip";
 
 export default function Trends() {
@@ -14,31 +11,7 @@ export default function Trends() {
       <div className="pt-[50px]">
         <HeroHeader />
         <HeroSection />
-
-        <div className="mb-12">
-          <NewsWidget />
-        </div>
-
-        <hr></hr>
-
-        <div className="mt-12 mb-12">
-          <HeatmapWidget />
-        </div>
-
-        <hr></hr>
-
-        <div className="mt-12 mb-12">
-          <ScreenerWidget />
-        </div>
-
-        <hr></hr>
-
-        <div className="mt-12 mb-12">
-          <EconomicCalendar />
-        </div>
-
-        <hr></hr>
-
+        <TrendsFeatures />
         <FooterSection />
       </div>
     </>

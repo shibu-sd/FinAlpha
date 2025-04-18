44bcc72b34b1ba937b9ac0ce5beeceacee3767e5
shibu-sd <sd.shibu007@gmail.com>
2025-04-18 19:57:51 +0530

    updated description text color

--- components/hero/trends-hero-section.tsx
+++ components/hero/trends-hero-section.tsx
@@ -2,7 +2,7 @@ import { ContainerTextFlip } from "../ui/container-text-flip";
 
 export default function HeroSection() {
     return (
-        <section className="bg-zinc-50 py-16 md:py-32 dark:bg-transparent">
+        <section className="bg-zinc-50 pt-16 pb-6 md:pt-32 md:pb-16 dark:bg-transparent">
             <div className="@container mx-auto max-w-5xl px-6">
                 <div className="text-center">
                     <h2 className="text-balance text-4xl font-semibold lg:text-5xl flex justify-center items-center gap-3 flex-wrap">
@@ -12,7 +12,7 @@ export default function HeroSection() {
                             textClassName="text-4xl lg:text-5xl"
                         />
                     </h2>
-                    <p className="mt-4">
+                    <p className="mt-4 text-muted-foreground">
                         Get a real-time pulse on the market with FinAlpha's all-in-one trends dashboard.
                     </p>
                 </div>

12cf488be62831ed07be61047d75cee260b96c1a
shibu-sd <sd.shibu007@gmail.com>
2025-07-06 18:48:22 +0530

    added the invest page

---
+++ app/invest/page.tsx
@@ -0,0 +1,13 @@
+import Features from "@/components/features/invest-features";
+import FooterSection from "@/components/footer";
+import { HeroHeader } from "@/components/hero/hero-header";
+
+export default function Tools() {
+    return (
+        <>
+            <HeroHeader />
+            <Features />
+            <FooterSection />
+        </>
+    );
+}

4b9938e7a90895cc1e7729dc56afe8f8aa2a367a
shibu-sd <sd.shibu007@gmail.com>
2026-01-26 14:35:05 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   trailingSlash: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? {
+      exclude: ['error', 'warn'],
+    } : false,
+  },
 };
 
 export default nextConfig;
